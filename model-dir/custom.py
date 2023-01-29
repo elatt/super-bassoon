f5ee1ee79d68e4ab2dc64bd029d86925cba6e132
Erik Lattimore <elatt@users.noreply.github.com>
2023-01-29 21:12:55 +0000

    Add new custom.py code that connects to DataRobot

--- model-dir/custom.py
+++ model-dir/custom.py
@@ -1,33 +1,135 @@
 """
-Copyright 2021 DataRobot, Inc. and its affiliates.
+Copyright 2023 DataRobot, Inc. and its affiliates.
 All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
+from datarobot_drum.runtime_parameters.runtime_parameters import RuntimeParameters
 import pandas as pd
+import requests
+
+# Load runtime params set by the platform
+PUBLIC_API_URL = RuntimeParameters.get('DATAROBOT_ENDPOINT') + '/api/v2'
+API_KEY = RuntimeParameters.get('DATAROBOT_API_KEY')['password']
+DEPLOYMENT_ID = RuntimeParameters.get('deploymentID')
+
+# Will be set
+API_URL = None
+DATAROBOT_KEY = None
+
+# Don't change this. It is enforced server-side too.
+MAX_PREDICTION_FILE_SIZE_BYTES = 52428800  # 50 MB
 
 
-def transform(data, model):
+class DataRobotPredictionError(Exception):
+    """Raised if there are issues getting predictions from DataRobot"""
+
+
+def make_datarobot_deployment_predictions(data, deployment_id):
     """
-    Note: This hook may not have to be implemented for your model.
-    In this case implemented for the model used in the example.
-
-    Modify this method to add data transformation before scoring calls. For example, this can be
-    used to implement one-hot encoding for models that don't include it on their own.
+    Make predictions on data provided using DataRobot deployment_id provided.
+    See docs for details:
+         https://app.datarobot.com/docs/predictions/api/dr-predapi.html
 
     Parameters
     ----------
-    data: pd.DataFrame
-    model: object, the deserialized model
+    data : str
+        If using CSV as input:
+        Feature1,Feature2
+        numeric_value,string
+
+        Or if using JSON as input:
+        [{"Feature1":numeric_value,"Feature2":"string"}]
+
+    deployment_id : str
+        The ID of the deployment to make predictions with.
 
     Returns
     -------
-    pd.DataFrame
+    Response schema:
+        https://app.datarobot.com/docs/predictions/api/dr-predapi.html#response-schema
+
+    Raises
+    ------
+    DataRobotPredictionError if there are issues getting predictions from DataRobot
     """
-    # Execute any steps you need to do before scoring
-    # Remove target columns if they're in the dataset
-    for target_col in ["Grade 2014", "Species"]:
-        if target_col in data:
-            data.pop(target_col)
-    data = data.fillna(0)
-    return data
+    # Set HTTP headers. The charset should match the contents of the file.
+    headers = {
+        # As default, we expect CSV as input data.
+        # Should you wish to supply JSON instead,
+        # comment out the line below and use the line after that instead:
+        'Content-Type': 'text/plain; charset=UTF-8',
+        # 'Content-Type': 'application/json; charset=UTF-8',
+
+        'Authorization': 'Bearer {}'.format(API_KEY),
+        'DataRobot-Key': DATAROBOT_KEY,
+    }
+
+    url = API_URL.format(deployment_id=deployment_id)
+
+    # Prediction Explanations:
+    # See the documentation for more information:
+    # https://app.datarobot.com/docs/predictions/api/dr-predapi.html#request-pred-explanations
+    # Should you wish to include Prediction Explanations or Prediction Warnings in the result,
+    # Change the parameters below accordingly, and remove the comment from the params field below:
+
+    params = {
+        # If explanations are required, uncomment the line below
+        # 'maxExplanations': 3,
+        # 'thresholdHigh': 0.5,
+        # 'thresholdLow': 0.15,
+        # For multiclass/clustering explanations only one of the 2 fields below may be specified
+        # Explain this number of top predicted classes in each row
+        # 'explanationNumTopClasses': 1,
+        # Explain this list of class names
+        # 'explanationClassNames': [],
+        # If text explanations are required, uncomment the line below.
+        # 'maxNgramExplanations': 'all',
+        # Uncomment this for Prediction Warnings, if enabled for your deployment.
+        # 'predictionWarningEnabled': 'true',
+    }
+    # Make API request for predictions
+    predictions_response = requests.post(
+        url,
+        data=data,
+        headers=headers,
+        # Prediction Explanations:
+        # Uncomment this to include explanations in your prediction
+        # params=params,
+    )
+    _raise_dataroboterror_for_status(predictions_response)
+    # Return a Python dict following the schema in the documentation
+    return predictions_response.json()
+
+
+def _raise_dataroboterror_for_status(response):
+    """Raise DataRobotPredictionError if the request fails along with the response returned"""
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        err_msg = '{code} Error: {msg}'.format(
+            code=response.status_code, msg=response.text)
+        raise DataRobotPredictionError(err_msg)
+
+
+### PASTE AN INTEGRATION SNIPPET FROM A DEPLOYMENT ABOVE ###
+def load_model(code_dir):
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer {}'.format(API_KEY),
+    }
+    return object()  # model placeholder
+
+
+def score(data, model, **kwargs):
+    # prepare the prediction request payload
+    payload = data.to_csv(index=False)
+
+    # make the prediction request
+    response = make_datarobot_deployment_predictions(payload, DEPLOYMENT_ID)
+
+    # convert the prediction request response to the required data structure
+    predictions = [item['prediction'] for item in response['data']]
+    predictions_data = pd.DataFrame({'Predictions': predictions})
+
+    return predictions_data
