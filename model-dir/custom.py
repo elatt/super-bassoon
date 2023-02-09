9705eaf3cc8aad165c5d65be53425b17a6e3f934
Erik Lattimore <elatt@users.noreply.github.com>
2023-02-09 13:50:50 -0500

    Fix and add param

--- model-dir/custom.py
+++ model-dir/custom.py
@@ -7,21 +7,26 @@ Released under the terms of DataRobot Tool and Utility Agreement.
 from datarobot_drum import RuntimeParameters
 
 
-def mask(value):
-    return value[:2] + ('*'*len(value[2:]))
+def mask(value, visible=3):
+    return value[:visible] + ("*" * len(value[visible:]))
 
 
 def transform(data, model):
     print("Loading the following Runtime Parameters:")
-    option1 = RuntimeParameters.get('option1')
-    option2 = RuntimeParameters.get('option2')
+    option1 = RuntimeParameters.get("option1")
+    option2 = RuntimeParameters.get("option2")
+    option3 = RuntimeParameters.get("option3")
     print(f"\toption1: {option1}")
     print(f"\toption2: {option2}")
-    
-    credential = RuntimeParameters.get('api_key')
+    print(f"\toption3: {option3}")
+
+    credential = RuntimeParameters.get("api_key")
     if credential is not None:
-        credential_type = credential.pop('credentialType')
-        print(f"\tapi_key(type={credential_type}): " + credential)
+        credential_type = credential.pop("credentialType")
+        print(
+            f"\tapi_key(type={credential_type}): "
+            + str({k: mask(v) for k, v in credential.items()})
+        )
     else:
         print("No credential data set")
     return data
