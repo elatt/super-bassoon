64bde79649210b0f61ffb1d5222ab74b871d9e04
Erik Lattimore <elatt@users.noreply.github.com>
2023-02-17 14:55:56 -0500

    Add more comments

--- model-dir/custom.py
+++ model-dir/custom.py
@@ -1,12 +1,15 @@
 """
-Copyright 2021 DataRobot, Inc. and its affiliates.
+Copyright 2023 DataRobot, Inc. and its affiliates.
 All rights reserved.
 This is proprietary source code of DataRobot, Inc. and its affiliates.
 Released under the terms of DataRobot Tool and Utility Agreement.
 """
+# Use this helper class to access the runtime parameter values in your model
 from datarobot_drum import RuntimeParameters
 
 
+# This is a naive function so as to not dump the full credential values
+# during this demonstration.
 def mask(value, visible=3):
     return value[:visible] + ("*" * len(value[visible:]))
 
@@ -14,13 +17,13 @@ def mask(value, visible=3):
 def transform(data, model):
     print("Loading the following Runtime Parameters:")
     option1 = RuntimeParameters.get("option1")
-    option2 = RuntimeParameters.get("option2")
-    option3 = RuntimeParameters.get("option3")
     print(f"\toption1: {option1}")
+    option2 = RuntimeParameters.get("option2")
     print(f"\toption2: {option2}")
+    option3 = RuntimeParameters.get("option3")
     print(f"\toption3: {option3}")
 
-    credential = RuntimeParameters.get("api_key")
+    credential = RuntimeParameters.get("encryption_key")
     if credential is not None:
         credential_type = credential.pop("credentialType")
         print(
@@ -29,4 +32,7 @@ def transform(data, model):
         )
     else:
         print("No credential data set")
+
+    # This transform function is just for illustrative purposes so just
+    # return the data back unaltered.
     return data
